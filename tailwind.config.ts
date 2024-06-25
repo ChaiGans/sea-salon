d9856557e993561d87cb5c1f209856e0fb13563b
ChaiGans <elbertchailes888@gmail.com>
2024-06-25 20:17:57 +0700

    add: public font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "custom-purple-1": "#411C3B",
+        "custom-purple-2": "#42243C",
+        "custom-purple-3": "#3D1F42",
+        "custom-purple-4": "#3C173C",
+        "custom-purple-5": "#531C43",
+      },
+      fontFamily: {
+        poppinsbold: ["Poppins-Bold", "sans-serif"],
+        poppinslight: ["Poppins-Light", "sans-serif"],
+        poppinsmedium: ["Poppins-Medium", "sans-serif"],
+        poppinsregular: ["Poppins-Regular", "sans-serif"],
+        poppinssb: ["Poppins-SemiBold", "sans-serif"],
+      },
     },
   },
   plugins: [],
