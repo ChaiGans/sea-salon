148c5b9442ed676164277ebc2ac11fecbed9499e
ChaiGans <elbertchailes888@gmail.com>
2024-06-28 09:51:41 +0700

    feat: review fe integrate with api

--- src/app/components/home/Review.tsx
+++ src/app/components/home/Review.tsx
@@ -11,47 +11,34 @@ import { ReviewPopOver } from "./ReviewPopOver";
 import { useToast } from "@/components/ui/use-toast";
 
 interface Review {
-  name: string;
+  customer_name: string;
   rating: number;
   comment: string;
 }
 
 const Review = () => {
-  const [testimonials, setTestimonials] = useState<Review[]>([
-    {
-      name: "Jane Doe",
-      rating: 5,
-      comment:
-        "SEA Salon offers the best haircut experience! The staff is friendly and professional. Highly recommend!",
-    },
-    {
-      name: "John Smith",
-      rating: 5,
-      comment:
-        "I had an amazing manicure and pedicure session. The atmosphere is so relaxing, and the service is top-notch!",
-    },
-    {
-      name: "Emily Johnson",
-      rating: 5,
-      comment:
-        "The facial treatments at SEA Salon are fantastic. My skin feels rejuvenated and glowing. Will definitely come back!",
-    },
-  ]);
+  const [testimonials, setTestimonials] = useState<Review[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/getReview");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const json = await response.json();
+      console.log(json.reviews);
+
+      setTestimonials(json.reviews as Review[]);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  };
 
   const { toast } = useToast();
 
   useEffect(() => {
-    if (testimonials.length > 3) {
-      toast({
-        title: "Review updated",
-        description: "Thanks for the review. Love you. -SEA Salon-",
-      });
-    }
-  }, [testimonials]);
-
-  const addReview = (review: Review) => {
-    setTestimonials((prevReviews) => [...prevReviews, review]);
-  };
+    fetchData();
+  }, []);
 
   return (
     <main className="min-h-screen w-full flex flex-col items-center justify-center">
@@ -65,21 +52,22 @@ const Review = () => {
         className="mb-4 w-3/4"
       >
         <CarouselContent>
-          {testimonials.map((item, index) => (
-            <CarouselItem
-              className="flex justify-center items-center"
-              key={index}
-            >
-              <ReviewCard
-                reviewerName={item.name}
-                starQuantity={item.rating}
-                reviewComment={item.comment}
-              />
-            </CarouselItem>
-          ))}
+          {testimonials.length > 0 &&
+            testimonials.map((item, index) => (
+              <CarouselItem
+                className="flex justify-center items-center"
+                key={index}
+              >
+                <ReviewCard
+                  reviewerName={item.customer_name}
+                  starQuantity={item.rating}
+                  reviewComment={item.comment}
+                />
+              </CarouselItem>
+            ))}
         </CarouselContent>
       </Carousel>
-      <ReviewPopOver addReview={addReview} />
+      <ReviewPopOver onPostSuccess={fetchData} />
     </main>
   );
 };
--- src/app/components/home/ReviewPopOver.tsx
+++ src/app/components/home/ReviewPopOver.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Popover,
   PopoverContent,
@@ -16,25 +17,60 @@ interface Review {
   comment: string;
 }
 
-interface ReviewPopOverProps {
-  addReview: (review: Review) => void;
+interface Props {
+  onPostSuccess: () => void;
 }
 
-export function ReviewPopOver({ addReview }: ReviewPopOverProps) {
+export const ReviewPopOver: React.FC<Props> = ({ onPostSuccess }) => {
   const [name, setName] = useState("John Doe");
   const [rating, setRating] = useState(5);
   const [comment, setComment] = useState("Cool. Love this place!");
+  const { toast } = useToast();
 
   const popoverCloseRef = useRef<HTMLButtonElement>(null);
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
     const reviewName = name.trim() === "" ? "Anonymous" : name;
     const newReview = { name: reviewName, rating: Number(rating), comment };
-    addReview(newReview);
+    // addReview(newReview);
 
-    setName("John Doe");
-    setRating(5);
-    setComment("Cool. Love this place!");
+    try {
+      const response = await fetch("/api/createReview", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          customer_name: name,
+          rating,
+          comment,
+        }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Something went wrong");
+      }
+
+      // Handle success
+      toast({
+        title: "Review added successfully",
+        description: "Thank you for leaving a review.",
+      });
+
+      onPostSuccess();
+
+      setName("John Doe");
+      setRating(5);
+      setComment("Cool. Love this place!");
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        title: "Registration Failed",
+        description: error.message,
+      });
+    }
 
     if (popoverCloseRef.current) {
       popoverCloseRef.current.click();
@@ -42,65 +78,68 @@ export function ReviewPopOver({ addReview }: ReviewPopOverProps) {
   };
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="destructive">Add your review</Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80 mt-2">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">Review</h4>
-            <p className="text-sm text-muted-foreground">
-              Give your honest review.
-            </p>
+    <form onSubmit={handleSubmit}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="destructive">Add your review</Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-80 mt-2">
+          <div className="grid gap-4">
+            <div className="space-y-2">
+              <h4 className="font-medium leading-none">Review</h4>
+              <p className="text-sm text-muted-foreground">
+                Give your honest review.
+              </p>
+            </div>
+            <div className="grid gap-2">
+              <div className="grid grid-cols-3 items-center gap-4">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  value={name}
+                  className="col-span-2 h-8"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+                <Label htmlFor="rating">Rating (1-5)</Label>
+                <Input
+                  id="rating"
+                  value={rating}
+                  min={1}
+                  max={5}
+                  className="col-span-2 h-8"
+                  onChange={(e) => setRating(Number(e.target.value))}
+                />
+              </div>
+              <div className="grid grid-cols-3 items-center gap-4">
+                <Label htmlFor="comment">Comment</Label>
+                <Input
+                  id="comment"
+                  value={comment}
+                  className="col-span-2 h-8"
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              </div>
+              <Button
+                className={`w-1/2 place-self-center mt-2 ${
+                  rating < 1 || rating > 5 ? "cursor-not-allowed" : ""
+                }`}
+                disabled={rating < 1 || rating > 5 ? true : false}
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Submit
+              </Button>
+              <PopoverClose asChild>
+                <button ref={popoverCloseRef} className="hidden">
+                  Close
+                </button>
+              </PopoverClose>
+            </div>
           </div>
-          <div className="grid gap-2">
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                value={name}
-                className="col-span-2 h-8"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="rating">Rating (1-5)</Label>
-              <Input
-                id="rating"
-                value={rating}
-                min={1}
-                max={5}
-                className="col-span-2 h-8"
-                onChange={(e) => setRating(Number(e.target.value))}
-              />
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="comment">Comment</Label>
-              <Input
-                id="comment"
-                value={comment}
-                className="col-span-2 h-8"
-                onChange={(e) => setComment(e.target.value)}
-              />
-            </div>
-            <Button
-              className={`w-1/2 place-self-center mt-2 ${
-                rating < 1 || rating > 5 ? "cursor-not-allowed" : ""
-              }`}
-              disabled={rating < 1 || rating > 5 ? true : false}
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-            <PopoverClose asChild>
-              <button ref={popoverCloseRef} className="hidden">
-                Close
-              </button>
-            </PopoverClose>
-          </div>
-        </div>
-      </PopoverContent>
-    </Popover>
+        </PopoverContent>
+      </Popover>
+    </form>
   );
-}
+};
